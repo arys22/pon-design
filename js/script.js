a4ae6b98d4a9ca897a631be462834d0df0e9e0bf
arys-s <87214988+arys-s@users.noreply.github.com>
2021-07-17 15:00:08 +0900

    scrollでheader変化

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 "use strict";
-
+new Vue({
+  el: "#app1",
+  data: {
+    ActiveBtn: false,
+    scrollY: 0,
+  },
+  mounted() {
+    // スクロール時 スクロール位置を取得
+    window.addEventListener("scroll", this.onScroll);
+    // ロード時
+    window.addEventListener('load', this.setCoordinate)
+  },
+  methods: {
+    // スクロールを検知
+    onScroll() {
+      this.scrollY = window.scrollY;
+    },
+  },
+});
