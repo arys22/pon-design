cc8e90208425ce8287d453d0f20acf699b0f7eed
arys-s <87214988+arys-s@users.noreply.github.com>
2021-07-19 15:30:37 +0900

    スライダー

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict";
-new Vue({
+// バーガー
+const vm1 = new Vue({
   el: "#app1",
   data: {
     ActiveBtn: false,
@@ -9,12 +10,48 @@ new Vue({
     // スクロール時 スクロール位置を取得
     window.addEventListener("scroll", this.onScroll);
     // ロード時
-    window.addEventListener('load', this.setCoordinate)
+    window.addEventListener("load", this.setCoordinate);
   },
   methods: {
     // スクロールを検知
     onScroll() {
       this.scrollY = window.scrollY;
+    },
+  },
+});
+
+// スライダー
+// vue-awesome-swiperを呼び出し
+const VueAwesomeSwiper = window.VueAwesomeSwiper;
+Vue.use(VueAwesomeSwiper);
+const vm2 = new Vue({
+  el: "#app2",
+  data: {
+    // options設定
+    swiperOption: {
+      // ループ
+      loop: true,
+      autoplay: {
+        // 自動再生5秒間隔
+        delay: 5000,
+        // 操作されても自動再生止めない
+        disableOnInteraction: false,
+      },
+      // フェードで切り替え
+      // effect: "fade",
+      // スライドの間隔
+      // spaceBetween: 0,
+      // ページネーション
+      pagination: {
+        el: ".swiper-pagination",
+        // クリック有効
+        clickable: true,
+      },
+      // 次のページ、前のページ
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     },
   },
 });
